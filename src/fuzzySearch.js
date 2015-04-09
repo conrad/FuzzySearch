2c2c2dc442db4709619e3cdaaf0fb57a7f0ccab9
Matt Conrad <mmconrad@gmail.com>
2015-04-09 10:08:33 -0700

    Begins working on levenshtein distance; Adds methods for using levenshtein

--- src/fuzzySearch.js
+++ src/fuzzySearch.js
@@ -1,4 +1,25 @@
 var fuzzySearch = function(input, dictionary){
   /* implement me! */
+  var result;
+  if (input in dictionary) {
+    result = input;
+  } else {
+    var bestDistance = Infinity;
+    var bestRelevance = Infinity;  // will be based on number given to words in dictionary object
+    for (var word in dictionary) {
+      var distance = levenshteinDistance(word, input);
+
+      if (distance < bestDistance) {
+        result = word;
+        bestDistance = distance;
+        bestRelevance = relevance;
+      } else if (distance === bestDistance && relevance < bestRelevance) {
+        result = word;
+        bestDistance = distance;
+        bestRelevance = relevance;
+      }
+    }
+  }
+  return result;
 }
 
--- src/levenshtein.js
+++ src/levenshtein.js
@@ -1,4 +1,47 @@
 var levenshteinDistance = function(word1, word2){
-  /* Implement me! */
+  var result = 0;
+  var grid = [];
+
+  for (var i = 0; i < word1.length; i++) {
+    grid.push([i]);
+  }
+  grid[0] = word2.split('').map(function(value, index){ return index; });
+
+  for (var i = 1; i < word1.length; i++) {
+    for (var j = 1; j < word2.length; j++) {
+
+      if (word1[i] === word2[j]) {
+        grid[i][j] = grid[i - 1][j - 1];
+      } else {
+        var above = grid[i - 1][j];
+        var left = grid[i][j - 1];
+        var aboveLeft = grid[i - 1][j - 1];
+      }
+
+
+      // if (word1[i] === word2[j]) {
+        // if they're the same, noop
+        // temp += word1[i];
+        // result += Math.abs(j-i);
+      // } else if () {
+      // }
+
+
+
+    }
+  }
+  return result;   // number of changes b/t words
 }
 
+
+
+  // add
+  // subtract
+  // replace
+
+  // for ()
+  // console.log(rows);
+
+
+
+// levenshteinDistance('hi', 'hey');
