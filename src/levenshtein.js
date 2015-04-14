2ac8809f802fc28c1746d128cd8d7949f833c30c
Matt Conrad <mmconrad@gmail.com>
2015-04-14 13:25:28 -0700

    Adds full levenshtein functionality

--- src/levenshtein.js
+++ src/levenshtein.js
@@ -1,47 +1,63 @@
-var levenshteinDistance = function(word1, word2){
-  var result = 0;
-  var grid = [];
+// var levenshteinDistance = function(word1, word2){
+//   var result = 0;
+//   var grid = [];
 
-  for (var i = 0; i < word1.length; i++) {
-    grid.push([i]);
+//   for (var i = 0; i < word1.length; i++) {
+//     grid.push([i]);
+//   }
+
+//   grid[0] = word2.split('').map(function(value, index){ return index; });
+
+//   for (var i = 1; i < word1.length; i++) {
+//     for (var j = 1; j < word2.length; j++) {
+
+//       if (word1[i] === word2[j]) {
+//         grid[i][j] = grid[i - 1][j - 1];
+//       } else {
+//         var above = grid[i - 1][j];
+//         var left = grid[i][j - 1];
+//         var aboveLeft = grid[i - 1][j - 1];
+//       }
+
+//     }
+//   }
+//   return result;   // # of changes b/t words
+// };
+
+// Compute the edit distance between the two given strings
+function levenshteinDistance(a, b) {
+  if(a.length === 0) return b.length; 
+  if(b.length === 0) return a.length; 
+ 
+  var matrix = [];
+ 
+  // increment along the first column of each row
+  var i;
+  for(i = 0; i <= b.length; i++){
+    matrix[i] = [i];
   }
-  grid[0] = word2.split('').map(function(value, index){ return index; });
-
-  for (var i = 1; i < word1.length; i++) {
-    for (var j = 1; j < word2.length; j++) {
-
-      if (word1[i] === word2[j]) {
-        grid[i][j] = grid[i - 1][j - 1];
+ 
+  // increment each column in the first row
+  var j;
+  for(j = 0; j <= a.length; j++){
+    matrix[0][j] = j;
+  }
+ 
+  // Fill in the rest of the matrix
+  for(i = 1; i <= b.length; i++){
+    for(j = 1; j <= a.length; j++){
+      if(b.charAt(i-1) == a.charAt(j-1)){
+        matrix[i][j] = matrix[i-1][j-1];
       } else {
-        var above = grid[i - 1][j];
-        var left = grid[i][j - 1];
-        var aboveLeft = grid[i - 1][j - 1];
+        matrix[i][j] = Math.min(matrix[i-1][j-1] + 1, // substitution
+                                Math.min(matrix[i][j-1] + 1, // insertion
+                                         matrix[i-1][j] + 1)); // deletion
       }
-
-
-      // if (word1[i] === word2[j]) {
-        // if they're the same, noop
-        // temp += word1[i];
-        // result += Math.abs(j-i);
-      // } else if () {
-      // }
-
-
-
     }
   }
-  return result;   // number of changes b/t words
-}
-
-
-
-  // add
-  // subtract
-  // replace
-
-  // for ()
-  // console.log(rows);
-
+ 
+  return matrix[b.length][a.length];
+};
 
 
 // levenshteinDistance('hi', 'hey');
